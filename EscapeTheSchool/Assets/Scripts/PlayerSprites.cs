e61186d6380027cb94c55dd429b5d97d7b741a2a
Sam P <33042752+SamP923@users.noreply.github.com>
2019-06-14 12:46:43 -0700

    sprite changes

---
+++ EscapeTheSchool/Assets/Scripts/PlayerSprites.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSprites : MonoBehaviour {
+
+	public Sprite[] playerSprites;
+	public GameObject player;
+	// Use this for initialization
+	void Awake () {
+		playerSprites = Resources.LoadAll<Sprite>("player");
+	}
+	void Start(){
+		if (GameObject.Find ("Player") != null) {
+			player = GameObject.Find ("Player");
+			player.AddComponent<SpriteRenderer>();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (GameObject.Find ("Player") != null) {
+			if (Input.GetAxis ("Vertical") > 0) {
+				player.GetComponent<SpriteRenderer>().sprite = playerSprites[4];
+			}
+			else if (Input.GetAxis ("Vertical") < 0) {
+				player.GetComponent<SpriteRenderer>().sprite = playerSprites[0];
+			}
+			else if (Input.GetAxis ("Horizontal") > 0) {
+				player.GetComponent<SpriteRenderer>().sprite = playerSprites[2];
+			}
+			else if (Input.GetAxis ("Horizontal") < 0) {
+				player.GetComponent<SpriteRenderer>().sprite = playerSprites[7];
+			}
+		}
+	}
+}
